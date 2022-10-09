42f97fd34a716b95a8ec351b6dfdb631a657219f
Oscar Gerardo Trejo Rivera <44709706+oscargerardo@users.noreply.github.com>
2022-10-09 18:37:51 -0500

    Ejemplo pdf

---
+++ acciones.js
@@ -0,0 +1,12 @@
+const pdf = document.querySelector('#botonPDF')
+
+pdf.addEventListener('click', ()=>{
+    var doc = new jsPDF();
+    doc.setFontSize(22);
+    doc.text(20, 20, 'This is a title');
+    
+    doc.setFontSize(16);
+    doc.text(20, 30, 'This is some normal sized text underneath.');
+    
+    doc.save('prueba.pdf');
+})
